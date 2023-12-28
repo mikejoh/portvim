6741c828d024fe874636c712afd81d0990a418a4
Mikael Johansson <mik.json@gmail.com>
2023-12-28 12:25:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# portvi(n|m) :wine_glass:
+
+Minimalistic nvim config using [`lazy`]() as package manager. Based on the amazing [typecraft](https://www.youtube.com/@typecraft_dev) nvim YT videos.
+
+_`portvin` translates to `port wine` in English._
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.opt.termguicolors = true
+vim.opt.autoindent = true
+vim.opt.copyindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.showmode = false
+vim.wo.relativenumber = true
+
